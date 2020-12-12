e096095eccf7904ffc282dfd6362ff2b49187a20
build <2243168976@qq.com>
2020-12-13 03:18:26 +0800

    judyln: support gaming mode performance control [1/2]

--- device.mk
+++ device.mk
@@ -76,5 +76,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Performance
+PRODUCT_PACKAGES += \
+    powerctl
+
 # common judy (sdm845)
 $(call inherit-product, device/lge/sdm845-common/sdm845.mk)
